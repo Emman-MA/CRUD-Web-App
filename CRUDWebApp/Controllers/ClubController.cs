ef5cad5a80bec93b8e5665b18b0746dd99598d75
Emmanuel Pascua <emmanuelpascua.ph@gmail.com>
2025-01-14 20:57:01 +0800

    Exploring Controllers

---
+++ CRUDWebApp/Controllers/ClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
