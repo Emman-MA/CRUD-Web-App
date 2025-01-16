021d0937fb0d6cf000c8befe471aef8c124e2461
Emmanuel Pascua <emmanuelpascua.ph@gmail.com>
2025-01-16 09:46:34 +0800

    Making the Views

--- CRUDWebApp/Controllers/RaceController.cs
+++ CRUDWebApp/Controllers/RaceController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUDWebApp.Data;
+using CRUDWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CRUDWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
