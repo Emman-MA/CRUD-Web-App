59dda0b67276e9f06d58dbe6d58b1805b0991dfa
Emmanuel Pascua <emmanuelpascua.ph@gmail.com>
2025-01-22 10:01:37 +0800

    Service Pattern

---
+++ CRUDWebApp/Interfaces/IClubService.cs
@@ -0,0 +1,15 @@
+﻿using CRUDWebApp.Models;
+
+namespace CRUDWebApp.Interfaces
+{
+    public interface IClubService
+    {
+        Task<IEnumerable<Club>> GetAll();
+        Task<Club> GetByIdAsync(int id);
+        Task<IEnumerable<Club>> GetClubByCity(string city);
+        bool Add(Club club);
+        bool Update(Club club);
+        bool Delete(Club club);
+        bool Save();
+    }
+}
---
+++ CRUDWebApp/Interfaces/IRaceService.cs
@@ -0,0 +1,15 @@
+﻿using CRUDWebApp.Models;
+
+namespace CRUDWebApp.Interfaces
+{
+    public interface IRaceService
+    {
+        Task<IEnumerable<Race>> GetAll();
+        Task<Race> GetByIdAsync(int id);
+        Task<IEnumerable<Race>> GetAllRacesByCity(string city);
+        bool Add(Race race);
+        bool Update(Race race);
+        bool Delete(Race race);
+        bool Save();
+    }
+}
